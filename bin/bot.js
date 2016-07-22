2d9031fe2ea1bb38237e864b38a767108a0525a3
Joao <jofisaes@hotmail.com>
2016-07-22 12:25:54 +0200

    initial bot setup

---
+++ bin/bot.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var Up2DateBot = require('../lib/up2datebot');
+
+var token = process.env.BOT_API_KEY || require('../token');
+var name = 'up2bot'
+
+var up2datebot = new Up2DateBot({
+    token: token,
+    name: name
+});
+
+up2datebot.run();
